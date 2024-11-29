62ce3b74f773ec2cdfb1f18c8a4091cb4bef2d41
Guy Phanvongsa <guy.phanvongsa@dpie.nsw.gov.au>
2024-11-29 16:54:30 +1100

    drop tables script if exists

---
+++ valnet.dataexporter/DbHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using Oracle.ManagedDataAccess.Client;
+
+namespace valnet.dataexporter;
+
+public class DbHelper
+{
+    private string connectionString;
+    private OracleConnection connection;
+    private const string sql_delim = "/";
+    public DbHelper(string connectionString)
+    {
+        this.connectionString = connectionString;
+        this.connection = new OracleConnection(connectionString);
+    }
+
+    public string generateScriptTable(string tableName) {
+        StringBuilder sbsql = new StringBuilder(generateScriptDropTable(tableName));
+        // TODO build table
+        return sbsql.ToString();
+    }
+
+    private string generateScriptDropTable(string tableName)
+    {
+        StringBuilder sbsql = new StringBuilder();
+        sbsql.AppendLine("BEGIN");
+        sbsql.AppendFormat("  -- Drop the {0} table if it exists\n", tableName);
+        sbsql.AppendFormat("  IF EXISTS (SELECT 1 FROM user_tables WHERE table_name = '{0}') THEN\n",tableName);
+        sbsql.AppendFormat("      EXECUTE IMMEDIATE 'DROP TABLE {0}';\n", tableName);
+        sbsql.AppendLine("  END IF;");
+        sbsql.AppendLine("END;");
+        sbsql.AppendLine(sql_delim);
+        return sbsql.ToString();
+    }
+}
--- valnet.dataexporter/Program.cs
+++ valnet.dataexporter/Program.cs
@@ -1,3 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+using System.Text;
+using valnet.dataexporter;
 
-Console.WriteLine("Hello, World!");
+// //var connectionString = "User Id=VALNET;Password=VALNET;Data Source=//oracle-host-11g:1521/XE";
+var connectionString = "User Id=VALNET;Password=VALNET;Data Source=//localhost:1520/XE";
+string tableName = "VN_PROPERTY";
+
+DbHelper dbhelper = new DbHelper(connectionString);
+Console.WriteLine(dbhelper.generateScriptTable(tableName));
+
+// Generate Create or Update Table Script
+//string script = GenerateCreateOrUpdateTableScript(connectionString, tableName);
+//Console.WriteLine(script);
+
+
+// Console.WriteLine("Initialising Connection");
+// //var connectionString = "User Id=VALNET;Password=VALNET;Data Source=//oracle-host-11g:1521/XE";
+// var connectionString = "User Id=VALNET;Password=VALNET;Data Source=//localhost:1520/XE";
+// using (var conn = new OracleConnection(connectionString)) {
+//     try
+//     {
+//         // Open connection to the database
+//         conn.Open();
+//         Console.WriteLine("Connected to Oracle Database");
+//
+//         // Define the SQL query to fetch tables
+//         string sqlQuery = "SELECT table_name FROM user_tables";
+//
+//         // Create an Oracle command
+//         using (OracleCommand cmd = new OracleCommand(sqlQuery, conn))
+//         {
+//             // Execute the query and get the result
+//             using (OracleDataReader reader = cmd.ExecuteReader())
+//             {
+//                 Console.WriteLine("Tables in the database:");
+//
+//                 // Loop through all tables and display their names
+//                 while (reader.Read())
+//                 {
+//                     Console.WriteLine(reader.GetString(0)); // Print table name
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     catch (Exception ex)
+//     {
+//         Console.WriteLine("Error: " + ex.Message);
+//     }
+// }
+
