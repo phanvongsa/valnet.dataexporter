6269374dcfb2d2c3c26bf2c7758c5b745dad2475
guy <guy.phanvongsa@dpie.nsw.gov.au>
2024-12-11 16:59:11 +1100

    enviromenst

--- valnet.dataexporter/Program.cs
+++ valnet.dataexporter/Program.cs
@@ -1,16 +1,27 @@
-﻿using System;
-//using Oracle.ManagedDataAccess.Client;
-using System.Text;
+﻿using Microsoft.Extensions.Configuration;
 using valnet.dataexporter;
 
+// This is a simple minimal console app without explicit class declaration
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "local";
+
+// Build the configuration based on environment
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true);
+
+var config = builder.Build();
+Console.WriteLine($"Application Name: {Directory.GetCurrentDirectory()}");
+Console.WriteLine($"Application Name: {config["env"]}");
+
 // //var connectionString = "User Id=VALNET;Password=VALNET;Data Source=//oracle-host-11g:1521/XE";
-var connectionString = "User Id=VALNET;Password=VALNET;Data Source=//localhost:1521/XE";
-string tableName = "VN_PROPERTY";
-
-DbHelper dbhelper = new DbHelper(connectionString);
-
-dbhelper.testConnection();
-Console.WriteLine(dbhelper.generateScriptTable(tableName));
+// var connectionString = "User Id=VALNET;Password=VALNET;Data Source=//localhost:1521/XE";
+// string tableName = "VN_PROPERTY";
+//
+// DbHelper dbhelper = new DbHelper(connectionString);
+//
+// dbhelper.testConnection();
+// Console.WriteLine(dbhelper.generateScriptTable(tableName));
 
 // Generate Create or Update Table Script
 //string script = GenerateCreateOrUpdateTableScript(connectionString, tableName);
